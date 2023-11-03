8f77611d974c033845d579da30370fa159afb026
mohammedaminu <aminuolawalekan@gmail.com>
2023-11-03 23:51:23 +0100

    ppm file

---
+++ 01.ppm_image.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+
+    int image_width = 2560;
+    int image_height = 2560;
+    ofstream ppmFile;
+    ppmFile.open("ppm_image.ppm");
+     
+        // Specify format and dimension of image
+        ppmFile
+        << "P3\n"
+        << image_width << ' ' << image_height << "\n255\n";
+    for (int j = 0; j < image_height; ++j)
+    {
+        clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_width - 1);
+            auto b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            ppmFile << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    clog << "\rDone.  \n";
+    return 0;
+}
